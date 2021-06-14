b47292b2eb07fc9e27a4ff9baba03ab791ddd3a7
Venterok <67825103+Venterok@users.noreply.github.com>
2021-06-14 10:32:39 +0300

    Rewrite joincheck

--- src/main/kotlin/org/venterok/advancedwhitelist/PlayerJoinCheck.kt
+++ src/main/kotlin/org/venterok/advancedwhitelist/PlayerJoinCheck.kt
@@ -3,16 +3,23 @@ package org.venterok.advancedwhitelist
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
-import kotlin.random.Random
 
 class PlayerJoinCheck : Listener {
     @EventHandler
     fun JoinCheck (e: AsyncPlayerPreLoginEvent) {
-        val whitelistEnabled = AdvancedWhitelist.inst?.config?.getBoolean("whitelist.enabled")
+
+        val config = AdvancedWhitelist.inst?.config
+
+        val whitelistEnabled = config?.getBoolean("whitelist.enabled")
+
         if (whitelistEnabled == false) return
+
         val joinedPlayer = e.name
-        val whitelistedPlayers = AdvancedWhitelist.inst?.config?.getStringList("in-whitelist")
-        val noWhitelistMessage = AdvancedWhitelist.inst?.config?.getString("message.join-no-whitelist") as String
+
+        val whitelistedPlayers = config?.getList("in-whitelist")
+
+        val noWhitelistMessage = config?.getString("message.join-no-whitelist") ?: return
+
         if (whitelistedPlayers != null) {
             if (whitelistedPlayers.contains(joinedPlayer)) return
             e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_OTHER, noWhitelistMessage)
